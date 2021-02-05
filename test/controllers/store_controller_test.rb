f67d898b864acc7dd7af3e802775e9f0c5a92f3d
Ling <lin@panter.ch>
2021-02-05 11:15:57 +0100

    feat: test and caching

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,5 +4,9 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select 'nav.side_nav a', minimum: 4
+    assert_select 'main ul.catalog li', 2
+    assert_select 'h2', 'MyString'
+    assert_select '.price', /\€[,\d]+\.\d\d/
   end
 end
